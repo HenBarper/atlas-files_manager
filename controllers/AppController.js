80f685f6ffb82c1aebd6a661d2ce43b5337a01d2
Ben Harper <benjaminitonio@gmail.com>
2024-03-14 14:57:08 -0500

    linted index.js, server.js, & AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,27 +1,27 @@
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
-
 class AppController {
   static async getStatus(req, res) {
     try {
       const redisAlive = await redisClient.isAlive();
       const dbAlive = await dbClient.isAlive();
-  
-      res.status(200).json({ "redis": redisAlive, "db": dbAlive });
+
+      res.status(200).json({ redisAlive, dbAlive });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
-  };
-  
+  }
+
   static async getStats(req, res) {
     try {
       const userCount = await dbClient.nbUsers();
       const filesCount = await dbClient.nbFiles();
+      res.status(200).json({ userCount, filesCount });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
-  };
+  }
 }
 
 module.exports = AppController;
