d4a476d2ed024c9e4d40364bd06648db191fbf83
sarahmarkland <sarah.markland@icloud.com>
2024-03-16 14:48:53 -0500

    task 3v1

---
+++ controllers/UsersController.js
@@ -0,0 +1,32 @@
+const User = require('../models/User');
+const sha1 = require('sha1');
+
+class UsersController = {
+  postUser: async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).send({ error: 'Missing email' });
+    }
+    if (!password) {
+      return res.status(400).send({ error: 'Missing password' });
+    }
+
+    try {
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).send({ error: 'Already exist' });
+      }
+
+      const sha1Password = sha1(password);
+      const newUser = new User({ email, password: sha1Password });
+      await newUser.save();
+      return res.status(201).json({ id: newUser._id, email: newUser.email });
+    } catch (error) {
+      console.error ("Error creating user: ", error);
+      return res.status(500).json({ error: "Internal service error" });
+    }
+  }
+};
+
+module.exports = UsersController;
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,16 @@ const router = express.Router();
 
 // Import the router for handling logic of routes
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 // Define status endpoint
 router.get('/status', AppController.getStatus);
 
 // Define stats endpoint
 router.get('/stats', AppController.getStats);
+
+// Define users endpoint
+router.post('/users', UsersController.postUser);
 
 // export the router
 module.exports = router;
