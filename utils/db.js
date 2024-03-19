6eae61bffa98f7d43fad06b8ae09e9ef20a467b7
Ben Harper <benjaminitonio@gmail.com>
2024-03-19 13:29:10 -0500

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,17 +1,32 @@
 const { MongoClient } = require('mongodb');
 
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+const dbURL = `mongodb://${host}:${port}/${database}`;
+
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'files_manager';
-    this.dbURL = `mongodb://${host}:${port}/${database}`;
-    this.client = new MongoClient(this.dbURL, { useUnifiedTopology: true });
-    this.client.connect();
+    MongoClient.connect(dbURL, { useUnifiedTopology: true }, (error, client) => {
+      if (client) {
+        this.db = client.db(database);
+        this.users = this.db.collection('users');
+        this.files = this.db.collection('files');
+      }
+      if (error) {
+        this.db = false;
+        console.log(error);
+      }
+    });
+    // this.client = new MongoClient(this.dbURL, { useUnifiedTopology: true });
+    // this.client.connect();
   }
 
   isAlive() {
-    return this.client.isConnected();
+    if (!this.db) {
+      return !!this.db;
+    }
+    return !!this.db;
   }
 
   async nbUsers() {
